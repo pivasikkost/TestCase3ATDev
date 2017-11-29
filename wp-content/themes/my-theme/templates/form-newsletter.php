5a8b30a631562dad294706c8b4f202940504134e
Konstantin Zosimenko <pivasikkost@gmail.com>
2017-11-29 12:36:52 +0300

    create newsletter partial

---
+++ wp-content/themes/my-theme/templates/form-newsletter.php
@@ -0,0 +1,4 @@
+<form action="http://test-case-3atdev.com/subscribe" method="post">
+  <input type="email" value="" name="email" placeholder="Your email address">
+  <button type="submit">Sign me up!</button>
+</form>
